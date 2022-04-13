d29349489dd420b0e7e2bc09ef267c026a65d1a0
EC2 Default User <ec2-user@ip-172-31-21-224.ec2.internal>
2022-04-13 09:41:32 +0000

    [Finish]screen_item

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,20 @@
 class BooksController < ApplicationController
   def new
      @book= Book.new
-     @books=Book.all
   end
 
   def create
-    book= Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book= Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+    render:new
+    end
   end
 
-  def show
-    @book = Book.find(params[:id])
+  def index
+     @books=Book.all
+      @book= Book.new
   end
 
   def show
@@ -19,16 +22,24 @@ class BooksController < ApplicationController
   end
 
   def update
-    book =Book.find(params[:id])
+    book= Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
 
+   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+   end
+
+   def edit
+    @book= Book.find(params[:id])
+   end
 
 private
 
   def book_params
     params.require(:book).permit(:title,:body)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-
-  get 'books/new'
-  get 'top'=>'homes#top'
-  post 'books' => 'books#create'
-  get 'books'=> 'books#new'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit',as: 'edit_book'
-  patch 'books/:id' => 'books#update',as: 'update_book'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ root 'homes#top'
+ resources :books
 end
-
